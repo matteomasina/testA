2574f66ec90981cbfef037670f5dfa633c4af970
matteomasina <matteomasina@gmail.com>
2014-07-29 10:02:37 +0200

    aggiornato apertura e chiusura task tramite activity esterna

--- src/com/example/testa/MyTaskDetailActivity.java
+++ src/com/example/testa/MyTaskDetailActivity.java
@@ -64,10 +64,8 @@ import android.widget.VideoView;
 
 public class MyTaskDetailActivity extends Activity {
 	private static final String MIME_TEXT_PLAIN = null;
-	private NfcAdapter mAdapter;
-    private PendingIntent pendingIntent;
-    private IntentFilter[] mIntentFilters;
-    private String[][] mNFCTechLists;
+	
+    
 	String nomeFile;
 	String id;
 	String stabilimentoText;
@@ -195,9 +193,6 @@ public class MyTaskDetailActivity extends Activity {
 	        @Override
 	        public void onClick(View v) {
 	        	String buttonText = btnStart.getText().toString();
-//	        	Toast.makeText(getApplicationContext(),
-//	        			buttonText, Toast.LENGTH_SHORT)
-//	                     .show();
 	        	switch(buttonText) {
 		            case "AVVIA LAVORAZIONE":
 		            	//btnStart.setText("TERMINA LAVORAZIONE");
@@ -255,88 +250,77 @@ public class MyTaskDetailActivity extends Activity {
 	        }
 	    });
 	
-	    mAdapter = NfcAdapter.getDefaultAdapter(this);
-		pendingIntent = PendingIntent.getActivity(this,0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+	    
 		
 	}
 	
-	@Override
-    public void onResume() {
-        super.onResume();
-        mAdapter.enableForegroundDispatch(this, pendingIntent, null, null);        
-    }
- 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mAdapter.disableForegroundDispatch(this);
-    }	
+
 	
 
-	@Override
-    public void onNewIntent(Intent intent) {
-        String action = intent.getAction();
-        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
- 
-        String s = action + "\n\n" + tag.toString();
- 
-        // parse through all NDEF messages and their records and pick text type only
-        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-        if (data != null) {
-            try {
-                for (int i = 0; i < data.length; i++) {
-                    NdefRecord [] recs = ((NdefMessage)data[i]).getRecords();
-                    byte[] k = recs [0].getPayload();
-                    String str = new String(k);
-                    Log.e("TagINFO", str);
-                    
-                    for (int j = 0; j < recs.length; j++) {
-                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
-                            Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
-                            byte[] payload = recs[j].getPayload();
-                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
-                            int langCodeLen = payload[0] & 0077;
- 
-                            s += ("\n\nNdefMessage[" + i + "], NdefRecord[" + j + "]:\n\"" +
-                                 new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1,
-                                 textEncoding) + "\"");
-                         
-                        }
-                    }
-                    if (str.indexOf("ko")!=-1) {
-                    	if (inizio) {                    	
-                    		Toast.makeText(this, "TAG NON CORRISPONDENTE A QUESTA STAZIONE", Toast.LENGTH_SHORT).show();
-                    		write_on_db("ANOMALIA TAG NON CORRISPONDENTE","AVVIO NON CONSENTITO :" + stabilimentoText ,"");
-                    		
-                    	} else {
-                    		Toast.makeText(this, "TAG NON CORRISPONDENTE A QUESTA STAZIONE", Toast.LENGTH_SHORT).show();
-                    		write_on_db("ANOMALIA TAG NON CORRISPONDENTE","TERMINE ATTIVITA' NON CONSENTITO :" + stabilimentoText ,"");
-                    		
-                    	}
-                    }
-                    if (str.indexOf("ok")!=-1) {   
-                    	if (inizio) { 
-                    		Toast.makeText(this, "TASK CORRETTAMENTE AVVIATO", Toast.LENGTH_SHORT).show();
-	    					write_on_db("IN LAVORAZIONE","TASK AVVIATO:" + stabilimentoText ,"IN LAVORAZIONE"); 
-	    					btnStart.setText("TERMINA LAVORAZIONE");
-	    					
-                    	} else {
-                    		Toast.makeText(this, "TASK CORRETTAMENTE CHIUSO", Toast.LENGTH_SHORT).show();
-                    		write_on_db("ESEGUITO","TASK CHIUSO :" + stabilimentoText ,"ESEGUITO");
-                    	
-                    	}
-    					//finish();            		
-                    } 
-                    if (str.indexOf("op")!=-1) {
-                    	Toast.makeText(this, "AVVICINARE TAG DISPOSITIVO NON TAG OPERATORE", Toast.LENGTH_SHORT).show();                       		            		
-                    }  
-                    
-                }                                 
-            } catch (Exception e) {
-                Log.e("TagDispatch", e.toString());
-            }
-        }		
-    }	
+//	@Override
+//    public void onNewIntent(Intent intent) {
+//        String action = intent.getAction();
+//        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+// 
+//        String s = action + "\n\n" + tag.toString();
+// 
+//        // parse through all NDEF messages and their records and pick text type only
+//        Parcelable[] data = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+//        if (data != null) {
+//            try {
+//                for (int i = 0; i < data.length; i++) {
+//                    NdefRecord [] recs = ((NdefMessage)data[i]).getRecords();
+//                    byte[] k = recs [0].getPayload();
+//                    String str = new String(k);
+//                    Log.e("TagINFO", str);
+//                    
+//                    for (int j = 0; j < recs.length; j++) {
+//                        if (recs[j].getTnf() == NdefRecord.TNF_WELL_KNOWN &&
+//                            Arrays.equals(recs[j].getType(), NdefRecord.RTD_TEXT)) {
+//                            byte[] payload = recs[j].getPayload();
+//                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+//                            int langCodeLen = payload[0] & 0077;
+// 
+//                            s += ("\n\nNdefMessage[" + i + "], NdefRecord[" + j + "]:\n\"" +
+//                                 new String(payload, langCodeLen + 1, payload.length - langCodeLen - 1,
+//                                 textEncoding) + "\"");
+//                         
+//                        }
+//                    }
+//                    if (str.indexOf("ko")!=-1) {
+//                    	if (inizio) {                    	
+//                    		Toast.makeText(this, "TAG NON CORRISPONDENTE A QUESTA STAZIONE", Toast.LENGTH_SHORT).show();
+//                    		write_on_db("ANOMALIA TAG NON CORRISPONDENTE","AVVIO NON CONSENTITO :" + stabilimentoText ,"");
+//                    		
+//                    	} else {
+//                    		Toast.makeText(this, "TAG NON CORRISPONDENTE A QUESTA STAZIONE", Toast.LENGTH_SHORT).show();
+//                    		write_on_db("ANOMALIA TAG NON CORRISPONDENTE","TERMINE ATTIVITA' NON CONSENTITO :" + stabilimentoText ,"");
+//                    		
+//                    	}
+//                    }
+//                    if (str.indexOf("ok")!=-1) {   
+//                    	if (inizio) { 
+//                    		Toast.makeText(this, "TASK CORRETTAMENTE AVVIATO", Toast.LENGTH_SHORT).show();
+//	    					write_on_db("IN LAVORAZIONE","TASK AVVIATO:" + stabilimentoText ,"IN LAVORAZIONE"); 
+//	    					btnStart.setText("TERMINA LAVORAZIONE");
+//	    					
+//                    	} else {
+//                    		Toast.makeText(this, "TASK CORRETTAMENTE CHIUSO", Toast.LENGTH_SHORT).show();
+//                    		write_on_db("ESEGUITO","TASK CHIUSO :" + stabilimentoText ,"ESEGUITO");
+//                    	
+//                    	}
+//    					//finish();            		
+//                    } 
+//                    if (str.indexOf("op")!=-1) {
+//                    	Toast.makeText(this, "AVVICINARE TAG DISPOSITIVO NON TAG OPERATORE", Toast.LENGTH_SHORT).show();                       		            		
+//                    }  
+//                    
+//                }                                 
+//            } catch (Exception e) {
+//                Log.e("TagDispatch", e.toString());
+//            }
+//        }		
+//    }	
 	
 	public void write_on_db(String operazione,String dettagli,String stato) {
 		URL url = null;
